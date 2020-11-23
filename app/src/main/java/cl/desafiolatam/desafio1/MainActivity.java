88a3d597791bdb22d71c342f5e74e26cf8deaab9
Patricio García A <pgarcia@serlitoral.cl>
2020-11-23 18:11:37 -0300

    Desafio Terminado

--- app/src/main/java/cl/desafiolatam/desafio1/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/desafio1/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Log_Main";
     private List<Integer> imagenes;
-
-
-
+    private TextView tvTitle;
+    private ImageButton replay;
 
     @BindView(R.id.textView) TextView textView;
     @BindView(R.id.imageViewLeft) ImageView imgLeft;
@@ -33,14 +35,26 @@ public class MainActivity extends AppCompatActivity {
         // TODO call server...
         //Log.d(TAG, "Pasó por el onClick");
         entregarObsequio();
+    }
 
+    @OnClick(R.id.btnRePlay) void reiniciar() {
+        imgResults.setVisibility(View.INVISIBLE);
+        imgLeft.setVisibility(View.VISIBLE);
+        imgCenter.setVisibility(View.VISIBLE);
+        imgRight.setVisibility(View.VISIBLE);
+        tvTitle.setText("Elige una de estas 3 cajas sorpresa y recibe un obsequio");
 
     }
 
     private void entregarObsequio() {
         Collections.shuffle(imagenes);
         imgResults.setImageResource(imagenes.get(0));
-        imgResults.setVisibility(textView.VISIBLE);
+        imgResults.setVisibility(View.VISIBLE);
+        imgLeft.setVisibility(View.INVISIBLE);
+        imgCenter.setVisibility(View.INVISIBLE);
+        imgRight.setVisibility(View.INVISIBLE);
+
+        tvTitle.setText("Felicitaciones, has ganado un premio");
     }
 
     @Override
@@ -48,7 +62,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        iniciarVista();
         cargarImagenes();
+
+    }
+
+    private void iniciarVista() {
+        tvTitle = findViewById(R.id.tvObsequio);
+        replay = findViewById(R.id.btnRePlay);
     }
 
     private void cargarImagenes() {
@@ -56,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
         imagenes.add(R.drawable.ic_gift_beach);
         imagenes.add(R.drawable.ic_gift_music);
         imagenes.add(R.drawable.ic_gift_pizza);
+        imagenes.add(R.drawable.ic_beber);
+        imagenes.add(R.drawable.ic_malteada);
+        imagenes.add(R.drawable.ic_pina_colada);
     }
 
 
